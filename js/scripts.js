e388b9a4d8df5de774a13db58697e9997d3aafc5
arkham <sidharthraj1995@gmail.com>
2021-08-05 16:32:16 -0600

    image viewer text test2

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,21 @@
     portfolio website.
 */
 
+//Image viewer   
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+}
+
+
 (function($) {
 
     // Remove no-js class
@@ -93,18 +108,6 @@
         });
     });
 
-    //Image viewer   
-    function myFunction(imgs) {
-        // Get the expanded image
-        var expandImg = document.getElementById("expandedImg");
-        // Get the image text
-        var imgText = document.getElementById("imgtext");
-        // Use the same src in the expanded image as the image being clicked on from the grid
-        expandImg.src = imgs.src;
-        // Use the value of the alt attribute of the clickable image as text inside the expanded image
-        imgText.innerHTML = imgs.alt;
-        // Show the container element (hidden with CSS)
-        expandImg.parentElement.style.display = "block";
-    }
+    
 
 })(jQuery);
